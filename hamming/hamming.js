1d5858706fe100cd506822ef41e42c42c1e35f00
JR <jroger2908@gmail.com>
2016-07-07 21:09:56 -0400

    All tests passing

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,10 +1,20 @@
-function Hamming(strand1, stand2) {
+function Hamming(strand1, strand2) {
   this.strand1 = strand1;
-  this.stand2 = stand2;
+  this.strand2 = strand2;
 }
 
-Hamming.prototype.compute = function() {
-  
+Hamming.prototype.compute = function(strand1, strand2) {
+  var str1 = strand1.split("");
+  var str2 = strand2.split("");
+  var diff = 0;
+  for (var i = 0; i < str1.length; i++) {
+    for (var i = 0; i < str2.length; i++) {
+      if (str1[i] !== str2[i]) {
+        diff ++;
+      }
+    }
+  }
+  return diff;
 }
 
 module.exports = Hamming;
